9595ab840a758efc347fc93b8e16259e0c0229cb
18PROGB0583 <18PROGB0583@NAS94.labs.cbtis94.local>
2019-11-15 14:01:00 -0600

    agrege la interfas de frmautor

---
+++ ProyectoU3/src/mysql/ProyectoU3.java
@@ -0,0 +1,14 @@
+
+package mysql;
+
+
+public class ProyectoU3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
